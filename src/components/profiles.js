92247083f5593c71438f589e460b35403aedd5c8
Roxana Chiorean <roxana.gherle@gmail.com>
2019-06-25 17:00:46 +0300

    Initial commit

---
+++ src/components/profiles.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Profile = (props) => {
+    return (
+        <div>
+            <Link to={{
+                pathname: `${props.match.url}/posts`
+            }}>take me to /profile/posts</Link>
+        </div>
+    )
+}
+
+export default Profile;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// COMPONENTS
+import Home from './components/home';
+import Posts from './components/posts';
+import Profile from './components/profiles';
+import PostItem from './components/post_item';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <header>
+                    <Link to='/'>Home</Link><br/>
+                    <Link to='/posts'>Posts</Link><br/>
+                    <Link to='/profile'>Profile</Link><br/>
+                    <hr/>
+                </header>
+                <Switch>
+                    <Route path='/posts/:id' component={PostItem} />
+                    <Route path='/profile' component={Profile} />
+                    <Route path='/posts' exact component={Posts} />
+                    <Route path='/' exact component={Home}/>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    )
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+)
